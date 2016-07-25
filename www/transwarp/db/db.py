42863250fb60d6805e9e54fc76deacb4ff62c3ab
fan.yang <dingfengbosushi@gmail.com>
2016-07-25 08:52:51 -0700

    finish db.py

--- www/transwarp/db/db.py
+++ www/transwarp/db/db.py
@@ -46,6 +46,38 @@ class _ConnectionCtx(object):
 def connection():
     return _conneactionCtx()
 
-class _ConnectionCtx():
+class _TransactionCtx(object):
+    def __enter__(self):
+        glob _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        return self
+
+    def __exit__(self,exctype,excvalue,traceback):
+        glob _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions == 0:
+                if exctype is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(self):
+        global _db_ctx
+        try:
+            _db_ctx.connection.commit()
+        except:
+            _db_ctx.connection.rollback()
+            raise
+    def rollback(self):
+        global _db_ctx
+        _db_ctx.connection.rollback()
  
  
